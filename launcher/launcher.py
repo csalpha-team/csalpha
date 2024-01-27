cdd4b3890a97a936099abbb05d5937c2e24c1805
lucaspcarames <lgpcarames@gmail.com>
2024-01-27 16:07:59 -0400

    add documentacao da classe concreta

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -2,6 +2,33 @@ from .abstract_launcher import LauncherBase
 import warnings
 
 class Launcher(LauncherBase):
+    """
+    Classe que implementa a coleta de dados para o lançamento
+    de um dado circuito. Os dados a serem coletados estão estritamente 
+    associados ao visto output do Netz.
+
+    Atributos
+    ---------
+    _dic_input : dict
+        Dicionário que armazena os dados de entrada para o lançamento
+        de um dado circuito. Os dados são armazenados em um dicionário
+        para facilitar a coleta de dados e a verificação dos mesmos.
+    
+    Métodos
+    -------
+    input_data(**kwargs)
+        Método que recebe os dados de entrada para o lançamento de um
+        dado circuito. Os dados são armazenados no dicionário _dic_input.
+    
+    remove_data(*args)
+        Método que remove os dados de entrada para o lançamento de um
+        dado circuito. Os dados são removidos do dicionário _dic_input.
+
+    check_data()
+        Método para verificar o status atual dos dados de lançamento,
+        retornando a relação de dados preenchidos e/ou não preenchidos.
+
+    """
     def __init__(self) -> None:
         self._dic_input = {
             "NomeAgenteVenda": None,
@@ -33,7 +60,24 @@ class Launcher(LauncherBase):
         
         }
     
-    def input_data(self, **kwargs):
+    def input_data(self, **kwargs) -> None:
+        """
+        Método que recebe os dados de entrada para o lançamento de um
+        dado circuito. Os dados são armazenados no dicionário _dic_input.
+
+        O dado de entrada deve estar associado a uma chave do dicionário, 
+        caso não esteja, um aviso será retornado e o dado não será armazenado.
+
+        O campo preenchido só pode ser atualizado uma única vez. Caso o campo
+        já esteja preenchido, o dado não será atualizado. Para atualizar o campo
+        é necessário remover o dado e inserir novamente.
+
+        Parâmetros
+        ----------
+        kwargs : dict
+            Dicionário com os dados de entrada para o lançamento de um dado
+            circuito. Os dados são armazenados no dicionário _dic_input.
+        """
         for key, value in kwargs.items():
             if key in self._dic_input.keys():
                 if not self._dic_input[key]:
@@ -46,6 +90,16 @@ class Launcher(LauncherBase):
 
 
     def remove_data(self, *args):
+        """
+        Método que remove os dados de entrada para o lançamento de um
+        dado circuito. Os dados são removidos do dicionário _dic_input.
+        
+        Parâmetros
+        ----------
+        args : tuple
+            Tupla com os dados de entrada para o lançamento de um dado
+            circuito. Os dados são removidos do dicionário _dic_input.
+        """
         for key in args:
             if key in self._dic_input.keys():
                     self._dic_input[key] = None
@@ -53,5 +107,14 @@ class Launcher(LauncherBase):
                 continue
 
 
-    def check_data(self) -> str:
+    def check_data(self) -> dict:
+        """
+        Método para verificar o status atual dos dados de lançamento,
+        retornando a relação de dados preenchidos e/ou não preenchidos.
+
+        Retorna
+        -------
+        dict
+            Dict com a relação de dados preenchidos e/ou não preenchidos.
+        """
         return self._dic_input
