7cec8ee606230efdc1bfb43ad80c4eef93dbd5ce
lucaspcarames <lgpcarames@gmail.com>
2024-05-13 12:54:26 -0400

    add: suggestions to PRL

--- matrices/matrices.py
+++ matrices/matrices.py
@@ -14,21 +14,27 @@ class Matrices(MatricesBase):
         """
         self.df = pd.read_excel(table_path)
         
-    
+    # TODO: Gerar funções de criação diferentes para matrizes valor e quantidade
+    # TODO: Setar as variáveis do eixo da matriz como input da função. Ex
     def create_matrices(df: pd.DataFrame, product: str, matrice_type:str):
             
-            
+        # Fazer um check geral em alguns typos nas definições das variáveis
         #NOTE: checks simples
         acepted_matrice_type_values = ['Valor', 'Quantidade']
-        acepted_produt_values = ['AcaiFruto']
+        acepted_produt_values = ['AcaiFruto'] 
+        
         
         if matrice_type not in acepted_matrice_type_values:
-            raise(f'Matrice Type value {matrice_type} is not accepted. Chose one of {acepted_type_values}')
+            raise(f'Matrice Type value {matrice_type} is not accepted. Chose one of {acepted_type_values}') # deveria passar o acepted_matrice_type_values, correto?
         
+        # TODO: Adicionar essa estrutura em um try-except
+        # TODO Adicionas lógica para diferenciar valor e quantidade na estrutura da matriz
+        # Eu me questiono sobre a real necessidade dessa checagem de produto e em levantar uma exceção caso o input não esteja presente
+        # nos produtos relacionados.
         if product not in acepted_produt_values:
             raise(f'Product value {product} is not accepted. Chose one of {acepted_produt_values}')
         
-        #filtra o df pro produto desejado
+        # filtra o df pro produto desejado
         df = df[df['Produto'] == product]
 
         #agrupa os dados
