ce25e9a8669e8f123db3997632225245583f5ba9
lucaspcarames <lgpcarames@gmail.com>
2024-06-01 15:07:46 -0300

    update: add general information about seller and buyer sector agent

--- matrices/matrices.py
+++ matrices/matrices.py
@@ -13,32 +13,38 @@ class Matrices(MatricesBase):
         Inicializa o DF
         """
         self.df = pd.read_excel(table_path)
+        self.seller_sector_agent = "SetorDoAgenteQueVendeI"
+        self.buyer_sector_agent = "SetorDoAgenteQueCompraI"
         
     # TODO: Gerar funções de criação diferentes para matrizes valor e quantidade
     # TODO: Setar as variáveis do eixo da matriz como input da função. Ex
-    def create_matrices(df: pd.DataFrame, product: str, matrice_type:str):
+    def create_matrices(self, df: pd.DataFrame, product: str, matrice_type:str):
             
         # Fazer um check geral em alguns typos nas definições das variáveis
-        #NOTE: checks simples
-        acepted_matrice_type_values = ['Valor', 'Quantidade']
-        acepted_produt_values = ['AcaiFruto'] 
+        # #NOTE: checks simples
+        # acepted_matrice_type_values = ['Valor', 'Quantidade']
+        # acepted_produt_values = ['AcaiFruto'] 
         
         
-        if matrice_type not in acepted_matrice_type_values:
-            raise(f'Matrice Type value {matrice_type} is not accepted. Chose one of {acepted_type_values}') # deveria passar o acepted_matrice_type_values, correto?
+        # if matrice_type not in acepted_matrice_type_values:
+        #     raise(f'Matrice Type value {matrice_type} is not accepted. Chose one of {acepted_type_values}') # deveria passar o acepted_matrice_type_values, correto?
         
-        # TODO: Adicionar essa estrutura em um try-except
-        # TODO Adicionas lógica para diferenciar valor e quantidade na estrutura da matriz
-        # Eu me questiono sobre a real necessidade dessa checagem de produto e em levantar uma exceção caso o input não esteja presente
-        # nos produtos relacionados.
-        if product not in acepted_produt_values:
-            raise(f'Product value {product} is not accepted. Chose one of {acepted_produt_values}')
+        # # TODO: Adicionar essa estrutura em um try-except
+        # # TODO Adicionas lógica para diferenciar valor e quantidade na estrutura da matriz
+        # # Eu me questiono sobre a real necessidade dessa checagem de produto e em levantar uma exceção caso o input não esteja presente
+        # # nos produtos relacionados.
+        # if product not in acepted_produt_values:
+        #     raise(f'Product value {product} is not accepted. Chose one of {acepted_produt_values}')
         
         # filtra o df pro produto desejado
-        df = df[df['Produto'] == product]
+        # try:
+        if not df[df['Produto'] == product].empty:
+            df = df[df['Produto'] == product]
+        else:
+            raise(KeyError(f"The selected product {product} was not found in the dataframe."))
 
         #agrupa os dados
-        result_df = df.groupby(['SetorDoAgenteQueVendeI', 'SetorDoAgenteQueCompraI'])['Quantidade'].sum().reset_index()
+        result_df = df.groupby([self.seller_sector_agent, self.buyer_sector_agent])['Quantidade'].sum().reset_index()
         
         #cria proto matriz
         matrix_df = result_df.pivot_table(
