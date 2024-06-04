e7f396892e7ce6fb2f78ef9afa20f4b327f8b726
lucaspcarames <lgpcarames@gmail.com>
2024-06-03 21:05:45 -0300

    updated: create_matrices in Matrices class

--- matrices/matrices.py
+++ matrices/matrices.py
@@ -8,51 +8,64 @@ class Matrices(MatricesBase):
     #NOTE: v1 sera implementada usando a tbextensa.xls
     #TODO: integrar o fluxo de geração das matrizes com a classe tabela
     
-    def __init__(self, table_path):
+    def __init__(
+                 self,
+                #  table_path
+                 ):
         """
         Inicializa o DF
         """
-        self.df = pd.read_excel(table_path)
-        self.seller_sector_agent = "SetorDoAgenteQueVendeI"
-        self.buyer_sector_agent = "SetorDoAgenteQueCompraI"
+        # self.dataframe = pd.read_excel(table_path)
+        self.seller_sector_agent = "SetorDoAgenteQueVende"
+        self.buyer_sector_agent = "SetorDoAgenteQueCompra"
         
+    def _row_sum(self, row):
+        return row.sum()
+    
+
     # TODO: Gerar funções de criação diferentes para matrizes valor e quantidade
     # TODO: Setar as variáveis do eixo da matriz como input da função. Ex
-    def create_matrices(self, df: pd.DataFrame, product: str, matrice_type:str):
-            
-        # Fazer um check geral em alguns typos nas definições das variáveis
-        # #NOTE: checks simples
-        # acepted_matrice_type_values = ['Valor', 'Quantidade']
-        # acepted_produt_values = ['AcaiFruto'] 
-        
-        
-        # if matrice_type not in acepted_matrice_type_values:
-        #     raise(f'Matrice Type value {matrice_type} is not accepted. Chose one of {acepted_type_values}') # deveria passar o acepted_matrice_type_values, correto?
-        
-        # # TODO: Adicionar essa estrutura em um try-except
-        # # TODO Adicionas lógica para diferenciar valor e quantidade na estrutura da matriz
-        # # Eu me questiono sobre a real necessidade dessa checagem de produto e em levantar uma exceção caso o input não esteja presente
-        # # nos produtos relacionados.
-        # if product not in acepted_produt_values:
-        #     raise(f'Product value {product} is not accepted. Chose one of {acepted_produt_values}')
-        
-        # filtra o df pro produto desejado
-        # try:
+    def create_matrices(self, df: pd.DataFrame, product: str, matrice_type:str, aggregate_method: str):
+
+        # Setting matrice type
+        self.matrice_type = matrice_type # It must be present in the dataframe
+
         if not df[df['Produto'] == product].empty:
             df = df[df['Produto'] == product]
         else:
             raise(KeyError(f"The selected product {product} was not found in the dataframe."))
 
         #agrupa os dados
-        result_df = df.groupby([self.seller_sector_agent, self.buyer_sector_agent])['Quantidade'].sum().reset_index()
+        if aggregate_method=='sum':
+            result_df = df.groupby([self.seller_sector_agent, self.buyer_sector_agent])[self.matrice_type].sum().reset_index()
+        elif aggregate_method=='mean':
+            result_df = df.groupby([self.seller_sector_agent, self.buyer_sector_agent])[self.matrice_type].mean().reset_index()
+        elif aggregate_method=='median':
+            result_df = df.groupby([self.seller_sector_agent, self.buyer_sector_agent])[self.matrice_type].median().reset_index()
+        else:
+            raise(ValueError(f"The selected aggregate method was not valid: {aggregate_method}. Please select or 'sum' or 'mean' or 'median'"))
         
+        # Select the unique sectors from buyer and seller sector agent
+        unique_sectors_seller = result_df[self.seller_sector_agent].unique()
+        unique_sectors_buyer = result_df[self.buyer_sector_agent].unique()
+
+        unique_sectors = sorted(set(unique_sectors_seller).union(set(unique_sectors_buyer)))
+
         #cria proto matriz
         matrix_df = result_df.pivot_table(
-            index='SetorDoAgenteQueCompraI', 
-            columns='SetorDoAgenteQueVendeI', 
-            values='Quantidade', 
+            index=self.seller_sector_agent, 
+            columns=self.buyer_sector_agent, 
+            values=self.matrice_type,
             fill_value=0 
-        )
+        ).reindex(index=unique_sectors, columns=unique_sectors, fill_value=0)
+
+        total_bought = pd.DataFrame(matrix_df.apply(self._row_sum, axis=0).to_dict(), index=[f"Total{self.matrice_type}Bought"])
+        matrix_df = pd.concat([matrix_df, total_bought])
+        matrix_df.index.name = self.seller_sector_agent
+        matrix_df.columns.name = self.buyer_sector_agent
+
+        matrix_df[f"Total{self.matrice_type}Selled"] = matrix_df.apply(self._row_sum, axis=1)
+
+        matrix_df[f"Total{self.matrice_type}Selled"][f"Total{self.matrice_type}Bought"] = None
         
         return matrix_df 
-            
